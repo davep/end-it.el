a616d4cebf6733fde58f6f36076dc093db30cbf6
Dave Pearson <davep@davep.org>
2018-06-22 16:42:47 +0100

    :bulb: We pick a comment style, not a character

--- end-it.el
+++ end-it.el
@@ -25,7 +25,7 @@
 ;; end-it.el is a simple tool that provides a command for quickly adding a
 ;; "foo.xxx ends here" marker to the end of the current buffer. The format
 ;; used to add it takes into account the language in use and attempts to use
-;; the correct comment character.
+;; the correct comment style.
 
 ;;; Code:
 
