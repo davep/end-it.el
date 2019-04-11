000fcc0912add7ff317a16ae5e61202162a80178
Dave Pearson <davep@davep.org>
2019-04-11 09:01:11 +0100

    :sparkles: Add support for perl-mode
    *shudder*

--- end-it.el
+++ end-it.el
@@ -35,7 +35,7 @@
          ";;; %s")
         ((derived-mode-p 'python-mode 'ruby-mode 'makefile-gmake-mode
                          'makefile-bsdmake-mode 'sh-mode 'restclient-mode
-                         'conf-mode 'yaml-mode 'dockerfile-mode)
+                         'conf-mode 'yaml-mode 'dockerfile-mode 'perl-mode)
          "### %s")
         ((derived-mode-p 'c-mode 'css-mode 'js-mode)
          "/* %s */")
