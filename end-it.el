b15c588ff59d57755ce64f8581a57d2591a38066
Dave Pearson <dave.pearson@synpromics.com>
2019-05-03 09:09:46 +0100

    :sparkles: Add julia-mode

--- end-it.el
+++ end-it.el
@@ -36,7 +36,7 @@
         ((derived-mode-p 'python-mode 'ruby-mode 'makefile-gmake-mode
                          'makefile-bsdmake-mode 'sh-mode 'restclient-mode
                          'conf-mode 'yaml-mode 'dockerfile-mode 'perl-mode
-                         'gitconfig-mode)
+                         'gitconfig-mode 'julia-mode)
          "### %s")
         ((derived-mode-p 'c-mode 'css-mode 'js-mode)
          "/* %s */")
