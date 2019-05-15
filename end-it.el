0df361aed3a9c0ac74828239ca8a7741a7e45736
Dave Pearson <dave.pearson@synpromics.com>
2019-05-15 17:10:05 +0100

    :bookmark: v1.15

--- end-it.el
+++ end-it.el
@@ -2,7 +2,7 @@
 ;; Copyright 2018-2019 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.14
+;; Version: 1.15
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/end-it.el
 ;; Package-Requires: ((emacs "24"))
